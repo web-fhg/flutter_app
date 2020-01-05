1c6c996e5413332c520c4adadbdda2cd7c435725
web-fhg <2858406987@qq.com>
2020-01-05 23:43:54 +0800

    fisrt

---
+++ lib/cards/datas.dart
@@ -0,0 +1,6 @@
+class Datas{
+  String text;
+  String author;
+
+  Datas ({this.text,this.author});
+}
---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import './test/app.dart';
+
+void main() => runApp(Tests());
